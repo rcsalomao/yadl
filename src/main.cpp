e0726416d5b90cbe5d9af05098a9be0278f9c533
RCSalomao <rafael.c.salomao@gmail.com>
2021-11-24 01:54:56 -0300

    reorg

---
+++ src/main.cpp
@@ -0,0 +1,19 @@
+#include <memory>
+#include "yadl.hpp"
+
+int main() {
+    std::vector<std::unique_ptr<yadl::RV>> vec;
+
+    std::unique_ptr<yadl::RV> rv = std::make_unique<yadl::Poisson>(5.7);
+    // std::unique_ptr<yadl::Normal> rv = std::make_unique<yadl::Normal>(gsl_rng_default, 5, 1);
+    double samp = rv->sample();
+    printf("%.2f\n", samp);
+    printf("%.2f\n", rv->mean());
+    printf("%f\n", rv->stdv());
+    // printf("%s\n", gsl_rng_name(rv->m_rng));
+    printf("%f\n", rv->pdf(rv->mean()));
+    printf("%f\n", rv->pdf(samp));
+    printf("%f\n", rv->cdfP(9999));
+
+    return 0;
+};
