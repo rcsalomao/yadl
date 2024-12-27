92a07bcecc9e015d17669a300bddd1827053d29b
rcsalomao <rafael.c.salomao@gmail.com>
2024-12-27 12:58:59 -0300

    chore: update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
-# cmake -S . -B ./build
-make -C build -j$(nproc) --silent
+export LD_LIBRARY_PATH="$HOME/.local/lib/:$HOME/.local/bin_files/gcc/lib64/:$LD_LIBRARY_PATH"
 
+cmake -S . -B ./build
+make -C ./build -j8
 ./build/main
