d376d541819db9e5032fb028df7e5dc3c9b49ef2
rcsalomao <rafael.c.salomao@gmail.com>
2025-06-11 23:09:44 -0300

    fix: better handling for the rng object and random distribution classes

--- example/main.cpp
+++ example/main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+// #include <iostream>
 #include <memory>
+#include <print>
 
 #include "yadl.hpp"
 
@@ -11,61 +12,61 @@ auto stringify(Rng&& seq) {
                  return to_string(a);
              }) |
              std::ranges::views::join_with(',') | std::ranges::views::common;
-    return "{" + std::string(std::begin(b), std::end(b)) + "}";
+    return "[" + std::string(std::begin(b), std::end(b)) + "]";
 };
 
 int main() {
     yadl::RNG rng{};
     {
-        std::cout << std::endl;
-        printf("rng name: %s\n", gsl_rng_name(rng.get()));
+        std::println("");
+        std::println("rng name: {}", gsl_rng_name(rng.get()));
     }
 
     {
-        std::cout << std::endl;
+        std::println("");
         std::vector v{1, 2, 3, 4, 5};
-        std::cout << "v: " << stringify(v) << std::endl;
+        std::println("v: {}", stringify(v));
         yadl::shuffle(rng, v);
-        std::cout << "shuffled v: " << stringify(v) << std::endl;
+        std::println("shuffled v: {}", stringify(v));
     }
 
     {
-        std::cout << std::endl;
-        printf("yadl::Dice(12).sample(rng): %i\n", yadl::Dice(12).sample(rng));
+        std::println("");
+        std::println("yadl::Dice(12).sample(rng): {}",
+                     yadl::Dice(12).sample(rng));
     }
 
     {
-        std::cout << std::endl;
+        std::println("");
         std::vector<std::unique_ptr<yadl::RV>> v_rv;
         v_rv.push_back(std::make_unique<yadl::UniformInt>(23, 32));
         v_rv.push_back(std::make_unique<yadl::Poisson>(4.7));
         v_rv.push_back(std::make_unique<yadl::Normal>(5, 1));
         v_rv.push_back(std::make_unique<yadl::Weibull>());
         for (auto [i, rv] : v_rv | std::ranges::views::enumerate) {
-            printf("rv[%i]->sample(rng): %.3f\n", static_cast<int>(i),
-                   rv->sample(rng));
-            printf("rv[%i]->mean(): %.3f\n", static_cast<int>(i), rv->mean());
-            printf("rv[%i]->stdv(): %.3f\n", static_cast<int>(i), rv->stdv());
+            std::println("rv[{}]->sample(rng): {:.4f}", i, rv->sample(rng));
+            std::println("rv[{}]->mean(): {:.4f}", i, rv->mean());
+            std::println("rv[{}]->stdv(): {:.4f}", i, rv->stdv());
         }
     }
 
     {
-        std::cout << std::endl;
-        printf("yadl::Normal rv = yadl::Normal(15, 5);\n");
+        std::println("");
+        std::println("yadl::Normal rv = yadl::Normal(15, 5);");
         yadl::Normal rv = yadl::Normal(15, 5);
-        printf("double sample = rv.sample(rng);\n");
+        std::println("double sample = rv.sample(rng);");
         double sample = rv.sample(rng);
-        printf("rv.mean(): %.2f\n", rv.mean());
-        printf("rv.stdv(): %.2f\n", rv.stdv());
-        printf("rv.pdf(rv.mean()): %.2f\n", rv.pdf(rv.mean()));
-        printf("rv.cdf_P(33.60): %.4f\n", rv.cdf_P(33.60));
-        printf("rv.cdf_P(rv.mean()): %.2f\n", rv.cdf_P(rv.mean()));
-        printf("rv.cdf_P_inv(0.9999): %.2f\n", rv.cdf_P_inv(0.9999));
-        printf("sample: %.3f\n", sample);
-        printf("rv.pdf(): %.3f\n", rv.pdf(sample));
-        printf("rv.cdf_P(sample): %.3f\n", rv.cdf_P(sample));
-        printf("rv.cdf_P_inv(rv.cdf_P(sample)): %.3f\n",
-               rv.cdf_P_inv(rv.cdf_P(sample)));
+        std::println("rv.mean(): {:.4f}", rv.mean());
+        std::println("rv.stdv(): {:.4f}", rv.stdv());
+        std::println("rv.pdf(rv.mean()): {:.4f}", rv.pdf(rv.mean()));
+        std::println("rv.cdf_P(33.60): {:.4f}", rv.cdf_P(33.60));
+        std::println("rv.cdf_P(rv.mean()): {:.4f}", rv.cdf_P(rv.mean()));
+        std::println("rv.cdf_P_inv(0.9999): {:.4f}", rv.cdf_P_inv(0.9999));
+        std::println("random sample: {:.4f}", sample);
+        std::println("rv.pdf(): {:.4f}", rv.pdf(sample));
+        std::println("rv.cdf_P(random sample): {:.4f}", rv.cdf_P(sample));
+        std::println("rv.cdf_P_inv(rv.cdf_P(random sample)): {:.4f}",
+                     rv.cdf_P_inv(rv.cdf_P(sample)));
     }
 
     return 0;
